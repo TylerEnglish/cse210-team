1fd4b7be8300d150421e3539dc066da69ad2a918
Nightkiller0011 <joshuaellis152@gmail.com>
2022-02-03 14:57:22 -0700

    Director and main?

---
+++ unit03-jumper/Director.cs
@@ -0,0 +1,47 @@
+using System;
+
+namespace unit03_jumper
+{
+    class Director
+    {
+        bool isPlaying = true;
+        public Director()
+        {
+
+        }
+
+        public void StartGame()
+        {
+            while (isPlaying)
+            {
+                GetInputs();
+                DoUpdates();
+                DoOutputs();
+            }
+        }
+
+        public void GetInputs()
+        {
+
+        }
+
+    /// Updates
+        public void DoUpdates()
+        {
+            if (!isPlaying)
+            {
+                return;
+            }
+        }
+    
+    /// Asks player if they are still playing.
+        public void DoOutputs()
+        {
+            if (!isPlaying)
+            {
+                return;
+            }
+        }
+
+    }
+}
