a2c02c3b4a242d72764559b5f23a1bb51fb68ea6
Nightkiller0011 <joshuaellis152@gmail.com>
2022-02-03 17:07:03 -0700

    small work on interface

--- unit03-jumper/Interface.cs
+++ unit03-jumper/Interface.cs
@@ -4,9 +4,21 @@ namespace unit03_jumper
 {
     class Interface
     {
+        private string letterGuess = "";
         public Interface()
         {
             ;
+        }
+
+        public void ask()
+        {
+            Console.Write($"Guess a Letter [a-z]: ");
+            letterGuess = Console.ReadLine();
+        }
+        
+        public string getGuess()
+        {
+            return letterGuess;
         }
     }
 }
